c26d04d062fc06ebac004c199b902650b181284b
batfolx <victorvelea007@yahoo.com>
2021-08-22 22:58:42 -0400

    Add muuuuuch nicer tabs

--- lib/cctv_layout.dart
+++ lib/cctv_layout.dart
@@ -17,7 +17,6 @@ class CCTV extends StatefulWidget {
 }
 
 class _CCTVState extends State<CCTV> {
-
   CCTVArgs args;
   int currCamera = 1;
   IOWebSocketChannel channel;
@@ -27,141 +26,194 @@ class _CCTVState extends State<CCTV> {
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
     if (args == null) {
       args = ModalRoute.of(context).settings.arguments as CCTVArgs;
-      channel = IOWebSocketChannel.connect("ws://${args.addr}:${args.port}/api/stream/user", headers: {
-        'cookie': sessionId,
-        'cameraNumber': args.camnum
-      });
+      channel = IOWebSocketChannel.connect(
+          "ws://${args.addr}:${args.port}/api/stream/user",
+          headers: {'cookie': sessionId, 'cameraNumber': args.camnum});
       currCamera = int.parse(args.camnum);
     }
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Camera $currCamera"),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () async {
-                TextEditingController temp = new TextEditingController();
-                var result = await showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: TextField(
-                          controller: temp,
-                          decoration: InputDecoration(
-                              hintText: "Camera number",
-                              labelText: "Camera number",
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.black),
-                                  borderRadius: BorderRadius.circular(10))),
-                        ),
-                        actions: [
-                          FlatButton(
-                              onPressed: () {
-                                Navigator.pop(context, -1);
-                              },
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.error,
-                                    color: Colors.red,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text("Cancel")
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Camera $currCamera"),
+          bottom: PreferredSize(
+            preferredSize: AppBar().preferredSize,
+            child: TabBar(
+              isScrollable: true,
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(
+                  child: IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () async {
+                        TextEditingController temp =
+                            new TextEditingController();
+                        var result = await showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                content: TextField(
+                                  controller: temp,
+                                  decoration: InputDecoration(
+                                      hintText: "Camera number",
+                                      labelText: "Camera number",
+                                      border: OutlineInputBorder(
+                                          borderSide:
+                                              BorderSide(color: Colors.black),
+                                          borderRadius:
+                                              BorderRadius.circular(10))),
+                                ),
+                                actions: [
+                                  FlatButton(
+                                      onPressed: () {
+                                        Navigator.pop(context, -1);
+                                      },
+                                      child: Row(
+                                        children: [
+                                          Icon(
+                                            Icons.error,
+                                            color: Colors.red,
+                                          ),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text("Cancel")
+                                        ],
+                                      )),
+                                  FlatButton(
+                                      onPressed: () {
+                                        Navigator.pop(context, temp.text);
+                                      },
+                                      child: Row(
+                                        children: [
+                                          Icon(
+                                            Icons.check,
+                                            color: Colors.green,
+                                          ),
+                                          SizedBox(
+                                            width: 5,
+                                          ),
+                                          Text("Confirm")
+                                        ],
+                                      )),
                                 ],
-                              )),
-                          FlatButton(
-                              onPressed: () {
-                                Navigator.pop(context, temp.text);
-                              },
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    Icons.check,
-                                    color: Colors.green,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text("Confirm")
-                                ],
-                              )),
-                        ],
-                      );
-                    });
+                              );
+                            });
 
-                if (result != null && result != -1) {
-                  // try to parse result into a number
-                  try {
-                    currCamera = int.parse(result);
-                    var resp = await getDataHeaders("${args.url()}/api/stream/change", {
-                      "cookie": sessionId,
-                      "cameraNumber": currCamera.toString()
-                    } as Map<String, String>);
+                        if (result != null && result != -1) {
+                          // try to parse result into a number
+                          try {
+                            currCamera = int.parse(result);
+                            var resp = await getDataHeaders(
+                                "${args.url()}/api/stream/change",
+                                {
+                                  "cookie": sessionId,
+                                  "cameraNumber": currCamera.toString()
+                                } as Map<String, String>);
 
-                    print(resp);
-                    setState(() {
-                    });
-                  } catch (e) {
-                    // do nothing for now
+                            print(resp);
+                            setState(() {});
+                          } catch (e) {
+                            // do nothing for now
 
-                  }
-                }
-              }),
-          IconButton(
-              icon: Icon(Icons.face),
-              onPressed: () async {
-                var resp = await getDataHeaders("${args.url()}/api/stream/facialDetect/on", {
-                  "cookie": sessionId,
-                  "cameraNumber": currCamera.toString()
-                } as Map<String, String>);
-              }),
-          IconButton(
-              icon: Icon(Icons.emoji_objects),
-              onPressed: () async {
-                var resp = await getDataHeaders("${args.url()}/api/stream/motionDetect/on", {
-                  "cookie": sessionId,
-                  "cameraNumber": currCamera.toString()
-                } as Map<String, String>);
-              }),
-          IconButton(
-              icon: Icon(Icons.cancel),
-              onPressed: () async {
-                var resp = await getDataHeaders("${args.url()}/api/stream/off", {
-                  "cookie": sessionId,
-                  "cameraNumber": currCamera.toString()
-                } as Map<String, String>);
-              }),
-
-        ],
+                          }
+                        }
+                      }),
+                ),
+                Tab(
+                  child: IconButton(
+                      icon: Icon(Icons.face),
+                      onPressed: () async {
+                        var resp = await getDataHeaders(
+                            "${args.url()}/api/stream/facialDetect/on",
+                            {
+                              "cookie": sessionId,
+                              "cameraNumber": currCamera.toString()
+                            } as Map<String, String>);
+                      }),
+                ),
+                Tab(
+                  child: IconButton(
+                      icon: Icon(Icons.emoji_objects),
+                      onPressed: () async {
+                        var resp = await getDataHeaders(
+                            "${args.url()}/api/stream/motionDetect/on",
+                            {
+                              "cookie": sessionId,
+                              "cameraNumber": currCamera.toString()
+                            } as Map<String, String>);
+                      }),
+                ),
+                Tab(
+                  child: IconButton(
+                      icon: Icon(Icons.hot_tub),
+                      onPressed: () async {
+                        var resp = await getDataHeaders(
+                            "${args.url()}/api/stream/heatMap/on",
+                            {
+                              "cookie": sessionId,
+                              "cameraNumber": currCamera.toString()
+                            } as Map<String, String>);
+                      }),
+                ),
+                Tab(
+                  child: IconButton(
+                      icon: Icon(Icons.video_call_rounded),
+                      onPressed: () async {
+                        var resp = await getDataHeaders(
+                            "${args.url()}/api/stream/normal/on",
+                            {
+                              "cookie": sessionId,
+                              "cameraNumber": currCamera.toString()
+                            } as Map<String, String>);
+                      }),
+                ),
+                Tab(
+                  child: IconButton(
+                      icon: Icon(Icons.cancel),
+                      onPressed: () async {
+                        var resp = await getDataHeaders(
+                            "${args.url()}/api/stream/off",
+                            {
+                              "cookie": sessionId,
+                              "cameraNumber": currCamera.toString()
+                            } as Map<String, String>);
+                      }),
+                )
+              ],
+            ),
+          ),
+          actions: [],
+        ),
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.black,
+            child: StreamBuilder(
+              stream: this.channel.stream,
+              builder: (context, snapshot) {
+                return snapshot.hasData
+                    ? Image.memory(
+                        snapshot.data,
+                        gaplessPlayback: true,
+                      )
+                    : getLoadingWidget();
+              },
+            )),
       ),
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          color: Colors.black,
-          child: StreamBuilder(
-            stream: this.channel.stream,
-            builder: (context, snapshot) {
-              return snapshot.hasData ? Image.memory(snapshot.data, gaplessPlayback: true,) : getLoadingWidget();
-            },
-          )),
     );
   }
 
   @override
   void dispose() {
-
     if (channel != null) channel.sink.close();
     super.dispose();
-
   }
 
   void setCameraNumber() {
@@ -171,9 +223,6 @@ class _CCTVState extends State<CCTV> {
       currCamera = 1;
     }
   }
-
-
-
 }
 
 class CCTVArgs {
@@ -184,7 +233,8 @@ class CCTVArgs {
   String password;
   String camnum;
 
-  CCTVArgs(this.proto, this.addr, this.port, this.login, this.password, this.camnum);
+  CCTVArgs(
+      this.proto, this.addr, this.port, this.login, this.password, this.camnum);
 
   String url() {
     return proto + "://" + addr + ":" + port;
